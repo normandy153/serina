0ee60fb6860102859eb00801f23a48275e1bca60
Steve Chan <normandy153@gmail.com>
2014-01-28 02:42:53 +1100

    Update docblock [SERINA-7]

--- modules/Core/Event/Waypoint/Node.php
+++ modules/Core/Event/Waypoint/Node.php
@@ -1,6 +1,9 @@
 <?php
 /**
- * Unencoded.php
+ * Node
+ *
+ * This represents a place in the world represented by an address. This is
+ * transcoded by Google Maps API for route plotting on a map canvas
  *
  * Date: 21/01/2014
  * Time: 10:58 PM
@@ -19,23 +22,18 @@ class Node {
 	private $address = '';
 
 	/**
-	 * Latitude of a waypoint
-	 *
-	 * @var string
-	 */
-	private $latitude = '';
-
-	/**
-	 * Longitude of a waypoint
-	 *
-	 * @var string
-	 */
-	private $longitude = '';
-
-	/**
 	 * Constructor
 	 */
 	public function __construct() {
+	}
+
+	/**
+	 * Convert this to a json_encode friendly format
+	 *
+	 * @return array|mixed
+	 */
+	public function jsonSerialize() {
+		return $this->getAddress();
 	}
 
 	/* Getters/Setters
@@ -61,46 +59,4 @@ class Node {
 	public function getAddress() {
 		return $this->address;
 	}
-
-	/**
-	 * Set latitude
-	 *
-	 * @param string $latitude
-	 * @return $this
-	 */
-	public function setLatitude($latitude) {
-		$this->latitude = $latitude;
-
-		return $this;
-	}
-
-	/**
-	 * Get latitude
-	 *
-	 * @return string
-	 */
-	public function getLatitude() {
-		return $this->latitude;
-	}
-
-	/**
-	 * Set longitude
-	 *
-	 * @param string $longitude
-	 * @return $this
-	 */
-	public function setLongitude($longitude) {
-		$this->longitude = $longitude;
-
-		return $this;
-	}
-
-	/**
-	 * Get longitude
-	 *
-	 * @return string
-	 */
-	public function getLongitude() {
-		return $this->longitude;
-	}
-} 
+}
