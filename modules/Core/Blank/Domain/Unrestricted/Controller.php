8f3df4b837f5e2b72ecc2da74028501c160612e0
Steve Chan <normandy153@gmail.com>
2014-01-28 23:12:57 +1100

    Add blank module [SERINA-9]

---
+++ modules/Core/Blank/Domain/Unrestricted/Controller.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * New
+ *
+ * Date: 28/01/2014
+ * Time: 11:07 PM
+ */
+
+namespace Core\Blank\Domain\Unrestricted;
+
+
+class Controller extends \App\Controller\Domain\Unrestricted {
+
+	/**
+	 * Stub method
+	 */
+	public function getTest() {
+		$this->output('getTest', array(
+		));
+	}
+}
