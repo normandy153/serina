fa3a663772af614c93ac1015f0c9eb59dcfc5a29
Steve Chan <normandy153@gmail.com>
2014-02-05 23:34:04 +1100

    Add users to event detail [SERINA-9]

--- modules/Core/Event/Event.php
+++ modules/Core/Event/Event.php
@@ -144,10 +144,7 @@ class Event {
 		/* Attendees
 		 */
 		$attendees = new \App\Collection();
-		$attendees->add(array(
-			'firstname' => 'Test Firstname',
-			'lastname' => 'Test Lastname',
-		));
+		$attendees->add($this->tempSpawnUser());
 
 		/* People
 		 */
@@ -161,6 +158,97 @@ class Event {
 		$this->setMarkers($markerCollection);
 		$this->setAttendees($attendees);
 		$this->setLeaders($leaders);
+	}
+
+	/**
+	 * Spawn a temporary user
+	 * @deprecated
+	 *
+	 * @return User
+	 */
+	private function tempSpawnUser() {
+
+		/* Define State
+		 */
+		$state = new \Core\User\State();
+		$state->setAbbreviation('VIC');
+		$state->setName('Victoria');
+
+		/* Define Country
+		 */
+		$country = new \Core\User\Country();
+		$country->setAbbreviation('AU');
+		$country->setName('Australia');
+
+		/* Define Full Address
+		 */
+		$address = new \Core\User\Address();
+		$address->setAddress1('Unit 1');
+		$address->setAddress2('10 Test St');
+		$address->setSuburb('Test Suburb');
+		$address->setState($state);
+		$address->setPostcode('1234');
+		$address->setCountry($country);
+
+		/* Define Gender
+		 */
+		$gender = new \Core\User\Gender('M');
+
+		/* Define Phone numbers
+		 */
+		$phone = new \Core\User\Phone();
+		$phone->setNumber('02 1234 5678');
+
+		/* Define Mobile numbers
+		 */
+		$mobile = new \Core\User\Phone();
+		$mobile->setNumber('0111 234 567');
+
+		/* Define Email
+		 */
+		$email = new \Core\User\Email();
+		$email->setAddress('test@test.com');
+
+		/* Define Emergency Contact info
+		 */
+		$contactPhone = new \Core\User\Phone();
+		$contactPhone->setNumber('5555 6666');
+
+		$contactMobile = new \Core\User\Phone();
+		$contactMobile->setNumber('7777 8888');
+
+		$contact = new \Core\User();
+		$contact->setFirstname('Joe');
+		$contact->setLastname('Smith');
+		$contact->setPhone($contactPhone);
+		$contact->setMobile($contactMobile);
+
+		/* Define account
+		 */
+		$account = new \Core\User\Account();
+		$account->setUsername('smithj');
+		$account->setPassword($account->encode('smithjpassword'));
+		$account->setActivationDate('2014-02-03 22:10:01');
+		$account->setExpiryDate('2015-02-03 22:10:01');
+
+		/* Define test User
+		 */
+		$user = new \Core\User();
+		$user->setUuid(crypt(uniqid() . sha1(microtime())));
+		$user->setFirstname('John');
+		$user->setLastname('Smith');
+		$user->setBirthdate('1990-11-07');
+		$user->setGender($gender);
+		$user->setAddress($address);
+		$user->setPhone($phone);
+		$user->setMobile($mobile);
+		$user->setEmail($email);
+		$user->setEmergencyContact($contact);
+		$user->setAccount($account);
+		$user->setCreatedAt('2010-06-19 14:45:34');
+		$user->setUpdatedAt('2010-06-19 14:45:34');
+
+		return $user;
 	}
 
 	/* Getters/Setters
