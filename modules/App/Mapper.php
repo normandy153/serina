f2e70874695d5456b93d0df176dfb58721b3f11f
Steve Chan <normandy153@gmail.com>
2014-04-06 13:46:17 +1000

    Add automatic model hydration [SERINA-10]
    This automatically maps properties to db columns as specified in
    properties()

--- modules/App/Mapper.php
+++ modules/App/Mapper.php
@@ -19,11 +19,66 @@ class Mapper {
 	private $database = null;
 
 	/**
+	 * The model used by this mapper for automatic hydration
+	 *
+	 * @var string
+	 */
+	protected $model = '';
+
+	/**
+	 * The database table used by this mapper for automatic hydration
+	 *
+	 * @var string
+	 */
+	protected $table = '';
+
+	/**
+	 * A list of class var/db column pairs
+	 *
+	 * @var array
+	 */
+	protected $properties = array();
+
+	/**
 	 * Constructor
 	 */
 	final public function __construct() {
 		$this->setDatabase(new Database());
+		$this->properties();
 		$this->setup();
+	}
+
+	/**
+	 * TODO: Move to individual class mappers
+	 */
+	protected function properties() {
+		$this->setModel('\\Core\\User');
+		$this->setTable('user');
+
+		$this->addProperty(array(
+			'property' => 'id',
+			'column' => 'id'
+		));
+
+		$this->addProperty(array(
+			'property' => 'uuid',
+			'column' => 'uuid'
+		));
+
+		$this->addProperty(array(
+			'property' => 'firstname',
+			'column' => 'firstname'
+		));
+
+		$this->addProperty(array(
+			'property' => 'lastname',
+			'column' => 'lastname'
+		));
+
+		$this->addProperty(array(
+			'property' => 'birthdate',
+			'column' => 'birthdate'
+		));
 	}
 
 	/**
@@ -31,6 +86,44 @@ class Mapper {
 	 */
 	protected function setup() {
 
+	}
+
+	/**
+	 * Add a property definition
+	 *
+	 * @param $property
+	 */
+	protected function addProperty($property) {
+		$this->properties[] = $property;
+	}
+
+	/**
+	 * Figure out which model setter to use
+	 *
+	 * @param $property
+	 * @return string
+	 */
+	protected function deriveMethod($property) {
+		return 'set' . ucwords($property);
+	}
+
+	/**
+	 * Populate the specified object
+	 *
+	 * @param $row
+	 */
+	protected function hydrate($row) {
+		$model = $this->getModel();
+		$instance = new $model();
+
+		foreach($this->getProperties() as $definition) {
+			$method = $this->deriveMethod($definition['property']);
+			$column = $definition['column'];
+
+			$instance->$method($row[$column]);
+		}
+
+		return $instance;
 	}
 
 	public function testQuery() {
@@ -44,8 +137,12 @@ class Mapper {
 
 		foreach($statement as $row) {
 			new \App\Probe($row);
+			new \App\Probe($this->hydrate($row));
 		}
 	}
+
+	/* Getters/Setters
+	 */
 
 	/**
 	 * Set database
@@ -63,5 +160,59 @@ class Mapper {
 	 */
 	protected function getDatabase() {
 		return $this->database;
+	}
+
+	/**
+	 * Set model
+	 *
+	 * @param string $model
+	 */
+	protected function setModel($model) {
+		$this->model = $model;
+	}
+
+	/**
+	 * Get model
+	 *
+	 * @return string
+	 */
+	protected function getModel() {
+		return $this->model;
+	}
+
+	/**
+	 * Set properties
+	 *
+	 * @param array $properties
+	 */
+	protected function setProperties($properties) {
+		$this->properties = $properties;
+	}
+
+	/**
+	 * Get properties
+	 *
+	 * @return array
+	 */
+	protected function getProperties() {
+		return $this->properties;
+	}
+
+	/**
+	 * Set table
+	 *
+	 * @param string $table
+	 */
+	protected function setTable($table) {
+		$this->table = $table;
+	}
+
+	/**
+	 * Get table
+	 *
+	 * @return string
+	 */
+	public function getTable() {
+		return $this->table;
 	}
 } 
