6359022753b7638abfe949265fdd5831d304707a
Steve Chan <normandy153@gmail.com>
2014-04-23 23:01:05 +1000

    Add mapper infrastructure for joins [SERINA-10]
    This should allow for dynamic mapping and object graph construction
    without having to manufacture it all manually, the long way.

--- modules/Core/User/UserMapper.php
+++ modules/Core/User/UserMapper.php
@@ -11,6 +11,8 @@ namespace Core\User;
 
 class UserMapper extends \App\Mapper {
 
+	protected $finalGraph = array();
+
 	/**
 	 * Define properties
 	 *
@@ -25,6 +27,65 @@ class UserMapper extends \App\Mapper {
 		$this->addProperty('firstname', 'firstname');
 		$this->addProperty('lastname', 'lastname');
 		$this->addProperty('birthdate', 'birthdate');
+
+		/* Join the Phone record onto the User record using
+		 * the following columns as a match. otherTable and
+		 * otherKey refer to the table and key being joined
+		 * onto this current mapper's model
+		 *
+		 * These definitions, when combined into the final
+		 * sql query, mustn't create a syntactically incorrect
+		 * query, or it'll just snap
+		 */
+		$this->addJoin('Phone', array(
+			'other' => array(
+				'table' => 'phone',
+				'alias' => 'p',
+				'key' => 'user_id'
+			),
+			'this' => array(
+				'alias' => 'u',
+				'key' => 'id',
+			)
+		));
+	}
+
+	public function addJoin($name, $item) {
+		$this->joins[$name] = $item;
+	}
+
+	// User u
+	public function from($thisModel, $alias) {
+		$this->graph[$this->getTable()] = array();
+
+		return "FROM `{$this->getTable()}` {$alias}";
+	}
+
+	// User Phone
+	public function join($otherModel) {
+		$otherTable = $this->joins[$otherModel]['other']['table'];
+		$otherAlias = $this->joins[$otherModel]['other']['alias'];
+		$otherKey = $this->joins[$otherModel]['other']['key'];
+		$thisTable = $this->getTable();
+		$thisAlias = $this->joins[$otherModel]['this']['alias'];
+		$thisKey = $this->joins[$otherModel]['this']['key'];
+
+		$str = "
+			LEFT JOIN {$otherTable} {$otherAlias}
+			ON {$otherAlias}.{$otherKey} = {$thisAlias}.{$thisKey}
+		";
+
+		$this->graph[$thisTable][$otherAlias] = '';
+
+		return $str;
+	}
+
+	public function build(\App\Collection $rowCollection) {
+
+		foreach($rowCollection as $currentInstance) {
+
+
+		}
 	}
 
 	/**
@@ -32,26 +93,14 @@ class UserMapper extends \App\Mapper {
 	 */
 	public function testQuery() {
 		$userMapper = $this;
-		$genderMapper = new \Core\User\GenderMapper();
-		$addressMapper = new \Core\User\AddressMapper();
-		$stateMapper = new \Core\User\StateMapper();
-		$countryMapper = new \Core\User\CountryMapper();
 		$phoneMapper = new \Core\User\PhoneMapper();
 
 		$query = "
 			SELECT
 				{$userMapper->select('u')},
-				{$genderMapper->select('g')},
-				{$addressMapper->select('a')},
-				{$stateMapper->select('s')},
-				{$countryMapper->select('c')},
 				{$phoneMapper->select('p')}
-			FROM `user` AS u
-			LEFT JOIN gender g ON u.gender_id = g.id
-			LEFT JOIN address a ON u.address_id = a.id
-			LEFT JOIN state s ON a.state = s.id
-			LEFT JOIN country c ON a.country = c.id
-			LEFT JOIN phone p ON u.id = p.user_id
+			{$userMapper->from('User', 'u')}
+			{$userMapper->join('Phone')}
 		";
 
 		$statement = $this->getDatabase()->prepare($query);
@@ -59,21 +108,15 @@ class UserMapper extends \App\Mapper {
 
 		foreach($statement as $row) {
 			$user = $userMapper->hydrate('u', $row);
-			$gender = $genderMapper->hydrate('g', $row);
-			$address = $addressMapper->hydrate('a', $row);
-			$state = $stateMapper->hydrate('s', $row);
-			$country = $countryMapper->hydrate('c', $row);
 			$phone = $phoneMapper->hydrate('p', $row);
 
-			new \App\Probe($phone);
+			$rowCollection = new \App\Collection();
+			$rowCollection->add($user);
+			$rowCollection->add($phone);
 
-			$address->setState($state);
-			$address->setCountry($country);
-
-			$user->setGender($gender);
-			$user->setAddress($address);
-
-			new \App\Probe($user);
+			$this->build($rowCollection);
 		}
+
+//		new \App\Probe($rowCollection);
 	}
 } 
