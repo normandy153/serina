e576945f83e7a3ff6107e43941303c80e8a3e081
Steve Chan <normandy153@gmail.com>
2014-04-26 05:11:38 +1000

    Add Gender join [SERINA-10]

--- modules/Core/User/UserMapper.php
+++ modules/Core/User/UserMapper.php
@@ -27,6 +27,7 @@ class UserMapper extends \App\Mapper {
 		$this->addProperty('birthdate', 'birthdate');
 		$this->addProperty('address', 'address_id');
 		$this->addProperty('phone', 'phone_id');
+		$this->addProperty('gender', 'gender_id');
 
 		/* Join the Phone record onto the User record using
 		 * the following columns as a match. otherTable and
@@ -63,6 +64,18 @@ class UserMapper extends \App\Mapper {
 				'key' => 'id',
 			),
 		));
+
+		$this->addJoin('Gender', array(
+			'this' => array(
+				'model' => '\Core\User\User',
+				'key' => 'gender_id',
+				'collection' => 'gender_id',
+			),
+			'other' => array(
+				'model' => '\Core\User\Gender',
+				'key' => 'id',
+			)
+		));
 	}
 
 	/**
@@ -70,11 +83,18 @@ class UserMapper extends \App\Mapper {
 	 */
 	public function findAll() {
 		$query = new Query();
-		$query->select('\Core\User\User u', '\Core\User\Address a', '\Core\User\State s', '\Core\User\Phone p')
+		$query->select(
+			'\Core\User\User u',
+			'\Core\User\Address a',
+			'\Core\User\State s',
+			'\Core\User\Phone p',
+			'\Core\User\Gender g'
+		)
 			->from('\Core\User\User u')
 			->leftJoin('\Core\User\User u', 'Address a')
 			->leftJoin('\Core\User\Address a', 'State s')
-			->leftJoin('\Core\User\User u', 'Phone p');
+			->leftJoin('\Core\User\User u', 'Phone p')
+			->leftJoin('\Core\User\User u', 'Gender g');
 
 		$statement = $this->getDatabase()->prepare($query->prepare());
 		$statement->execute();
@@ -85,11 +105,13 @@ class UserMapper extends \App\Mapper {
 		$addressMapper = $query->getMapper('\Core\User\Address', 'a');
 		$stateMapper = $query->getMapper('\Core\User\State', 's');
 		$phoneMapper = $query->getMapper('\Core\User\Phone', 'p');
+		$genderMapper = $query->getMapper('\Core\User\Gender', 'g');
 
 		$userCollection = new \App\Collection();
 		$addressCollection = new \App\Collection();
 		$stateCollection = new \App\Collection();
 		$phoneCollection = new \App\Collection();
+		$genderCollection = new \App\Collection();
 
 		foreach($statement as $row) {
 			$user = $userMapper->hydrate('u', $row);
@@ -103,6 +125,9 @@ class UserMapper extends \App\Mapper {
 
 			$phone = $phoneMapper->hydrate('p', $row);
 			$phoneCollection->setItemAt($phone->getId(), $phone);
+
+			$gender = $genderMapper->hydrate('g', $row);
+			$genderCollection->setItemAt($gender->getId(), $gender);
 		}
 
 		/* Build address/state relation
@@ -116,6 +141,10 @@ class UserMapper extends \App\Mapper {
 		/* Build user/phone relation
 		 */
 		$this->joinCollections($userCollection, $phoneCollection, 'Phone', $query);
+
+		/* Build user/gender relation
+		 */
+		$this->joinCollections($userCollection, $genderCollection, 'Gender', $query);
 
 		return $userCollection;
 	}
