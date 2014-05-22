9237ec1b959c28f3999fe492764b0fdddd5db5eb
Steve Chan <normandy153@gmail.com>
2014-05-22 22:31:37 +1000

    Update Dropdown to support preselection and chainable [SERINA-12]

--- modules/App/Date/Dropdown.php
+++ modules/App/Date/Dropdown.php
@@ -2,6 +2,16 @@
 /**
  * Dropdown.php
  *
+ * Generate date values suitable for dropdown selections
+ *
+ * This is a mutable instance, so you can use:
+ *
+ * $dropdown->setMinYearOffset(0);
+ * $dropdown->setPreselectedDay(1);
+ *
+ * ...to alter the possible options from which you can choose.
+ * These are also chainable operations
+ *
  * Date: 21/05/2014
  * Time: 11:33 PM
  */
@@ -19,10 +29,30 @@ class Dropdown {
 	private $minYearOffset = 18;
 
 	/**
+	 * Preselected day
+	 *
+	 * @var int
+	 */
+	private $preselectedDay = -1;
+
+	/**
+	 * Preselected month
+	 *
+	 * @var int
+	 */
+	private $preselectedMonth = -1;
+
+	/**
+	 * Preselected year
+	 *
+	 * @var int
+	 */
+	private $preselectedYear = -1;
+
+	/**
 	 * Constructor
 	 */
 	public function __construct() {
-
 	}
 
 	/**
@@ -32,18 +62,32 @@ class Dropdown {
 	 */
 	public function generate() {
 		for ($days = array(), $i = 1; $i < 32; $i++) {
+			if ($i == $this->getPreselectedDay()) {
+				$selected = true;
+			}
+			else {
+				$selected = false;
+			}
+
 			$days[] = array(
 				'label' => $i,
 				'value' => $i,
-				'selected' => false,
+				'selected' => $selected,
 			);
 		}
 
 		for ($months = array(), $i = 1; $i < 13; $i++) {
+			if ($i == $this->getPreselectedMonth()) {
+				$selected = true;
+			}
+			else {
+				$selected = false;
+			}
+
 			$months[] = array(
 				'label' => date('M', mktime(0, 0, 0, $i)),
 				'value' => $i,
-				'selected' => false,
+				'selected' => $selected,
 			);
 		}
 
@@ -52,11 +96,18 @@ class Dropdown {
 		$minYear = date('Y')-$this->getMinYearOffset();
 
 		for ($years = array(), $i = 1900; $i <= $minYear; $i++) {
-			if ($i == $minYear) {
-				$selected = true;
+			$selected = false;
+
+			/* If a preselected value is provided, match that
+			 * Otherwise match it based on min year
+			 */
+			if ($this->getPreselectedYear() > -1) {
+				if ($i == $this->getPreselectedYear()) {
+					$selected = true;
+				}
 			}
-			else {
-				$selected = false;
+			else if ($i == $minYear) {
+				$selected = true;
 			}
 
 			$years[] = array(
@@ -80,9 +131,12 @@ class Dropdown {
 	 * Set min year offset
 	 *
 	 * @param int $minYearOffset
+	 * @return $this
 	 */
 	public function setMinYearOffset($minYearOffset) {
 		$this->minYearOffset = $minYearOffset;
+
+		return $this;
 	}
 
 	/**
@@ -92,5 +146,68 @@ class Dropdown {
 	 */
 	public function getMinYearOffset() {
 		return $this->minYearOffset;
+	}
+
+	/**
+	 * Set preselected day
+	 *
+	 * @param int $preselectedDay
+	 * @return $this
+	 */
+	public function setPreselectedDay($preselectedDay) {
+		$this->preselectedDay = $preselectedDay;
+
+		return $this;
+	}
+
+	/**
+	 * Get preselected day
+	 *
+	 * @return int
+	 */
+	public function getPreselectedDay() {
+		return $this->preselectedDay;
+	}
+
+	/**
+	 * Set preselected month
+	 *
+	 * @param int $preselectedMonth
+	 * @return $this
+	 */
+	public function setPreselectedMonth($preselectedMonth) {
+		$this->preselectedMonth = $preselectedMonth;
+
+		return $this;
+	}
+
+	/**
+	 * Get preselected month
+	 *
+	 * @return int
+	 */
+	public function getPreselectedMonth() {
+		return $this->preselectedMonth;
+	}
+
+	/**
+	 * Set preselected year
+	 *
+	 * @param int $preselectedYear
+	 * @return $this
+	 */
+	public function setPreselectedYear($preselectedYear) {
+		$this->preselectedYear = $preselectedYear;
+
+		return $this;
+	}
+
+	/**
+	 * Get preselected year
+	 *
+	 * @return int
+	 */
+	public function getPreselectedYear() {
+		return $this->preselectedYear;
 	}
 } 
