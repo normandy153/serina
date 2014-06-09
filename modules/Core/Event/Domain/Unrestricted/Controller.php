33ff6d7bca79ca38fa1461d5cbb1a2bc5eb20edc
Steve Chan <normandy153@gmail.com>
2014-06-09 18:36:08 +1000

    Add Event Create [SERINA-16]
    This only provides input for basic event details so far

--- modules/Core/Event/Domain/Unrestricted/Controller.php
+++ modules/Core/Event/Domain/Unrestricted/Controller.php
@@ -10,11 +10,24 @@ namespace Core\Event\Domain\Unrestricted;
 
 
 use App\Controller\Domain\Unrestricted;
+use App\Date\Dropdown;
 use App\Probe;
 use Core\Event;
 use Core\User\UserMapper;
 
 class Controller extends Unrestricted {
+
+	public function getEventCreate() {
+		$dropdown = new Dropdown();
+		$dropdown->setMinYearOffset(0);
+		$dropdown->setPreselectedYear(date('Y'));
+		$dropdown->setPreselectedMonth(date('n'));
+		$dropdown->setPreselectedDay(date('j'));
+
+		$this->output('getEventCreate', array(
+			'allDates' => $dropdown->generate(),
+		));
+	}
 
 	/**
 	 * Get a list of events
